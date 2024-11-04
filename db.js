c9ef83a87bbe9d1804b31526e6901d12880db244
octavioserrago <octavioserrago.dev@gmail.com>
2024-11-04 12:13:39 -0300

    Fixing

--- db.js
+++ db.js
@@ -1,14 +1,30 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+async function testConnection() {
+    try {
+        const connection = await pool.getConnection();
+        console.log('Connected to the database successfully!');
+        connection.release();
+    } catch (error) {
+        console.error('Error connecting to the database:', error.message);
+    }
+}
+
+testConnection();
 
 module.exports = pool;
