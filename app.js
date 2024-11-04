6132b0bdef024444c4adb23153c2f575c69fbc5d
octavioserrago <octavioserrago.dev@gmail.com>
2024-11-04 11:49:34 -0300

    trying to fix db connection

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const mysql = require('mysql2/promise'); // Importa mysql2/promise para la conexión
 
 const branchRoute = require('./src/routes/branchRoute');
 const userRoute = require('./src/routes/userRoute');
 const saleRoute = require('./src/routes/saleRoute');
 const blueprintRoute = require('./src/routes/blueprintRoute');
 const blueprintPhotosRoute = require('./src/routes/blueprintPhotosRoute');
+
 
 const app = express();
 const port = 4000;
@@ -23,42 +23,15 @@ app.use('/sales', saleRoute);
 app.use('/blueprints', blueprintRoute);
 app.use('/blueprintPhotos', blueprintPhotosRoute);
 
+
+
 app.get('/', (req, res) => {
     res.send('¡Bienvenido a mi aplicación!');
 });
 
-// Función para conectarse a la base de datos
-async function connectToDatabase() {
-    try {
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASSWORD // Si tienes contraseña
-        });
+app.listen(port, () => {
+    console.log(`Servidor iniciado`);
 
-        console.log('Conexión a la base de datos exitosa.');
-
-        // Realiza una consulta simple
-        const [rows] = await connection.query('SELECT NOW() AS currentTime');
-        console.log('Hora actual en la base de datos:', rows[0].currentTime);
-
-        // Cierra la conexión
-        await connection.end();
-    } catch (error) {
-        console.error('Error al conectarse a la base de datos:', error);
-    }
-}
-
-// Inicia el servidor y conecta a la base de datos
-app.listen(port, async () => {
-    console.log(`Servidor iniciado en http://localhost:${port}`);
-    console.log('Conectando a la base de datos con:', {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-    });
-
-    // Llama a la función para conectar a la base de datos
-    await connectToDatabase();
 });
+
+//node --env-file=.env --watch app.js
--- db.js
+++ db.js
@@ -2,13 +2,13 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
-    port: 3306
+    queueLimit: 0
 });
 
 module.exports = pool;
